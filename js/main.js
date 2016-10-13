ce7616bb9ab8e3ddd71dc43693145a514b15ba2f
NoNameGuy <paulojose.m.vieira@gmail.com>
2016-10-13 22:57:17 +0100

    .
    .

--- js/main.js
+++ js/main.js
@@ -9,10 +9,8 @@
     "use strict";
 
     $( document ).ready(function() {
-    // jQuery Code
-    //$('.col-xs-6 col-sm-4 text-center:last-child').hide();
-    //$('div[class='col-xs-6 col-sm-4 text-center']:last-child').hide();
-    $(".col-xs-6:last-child").hide()
+
+      $(".col-xs-6:last-child").hide() //ESCONDE O ULTIMO AUTOR
 
     });
 
