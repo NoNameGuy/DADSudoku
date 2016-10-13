846a14c23832d3674a4ab3c4003e882a831af09f
lostheaven92 <lostheaven92@gmail.com>
2016-10-13 23:37:22 +0100

    Function to change student photo
    Creation of the function changeStudentPhoto() to wrap image size.

--- js/main.js
+++ js/main.js
@@ -9,9 +9,22 @@
     "use strict";
 
     $( document ).ready(function() {
+      var NUM_STUDENTS = 3;
 
       $(".col-xs-6:last-child").hide() //ESCONDE O ULTIMO AUTOR
-
+      
+      for (var i = 1; i <= NUM_STUDENTS; i++)
+      	changeStudentPhoto(i);	
     });
+
+    function changeStudentPhoto(nrStudent){
+    	// Wrap photo in standard size
+    	var photoSize = 400;
+    	
+    	if(nrStudent < 1 || nrStudent > 3)
+    		return;
+    	
+    	$(".col-xs-6:nth-child("+(nrStudent+1)+") img").css('height', photoSize).css('width', photoSize);
+    }
 
 })();
